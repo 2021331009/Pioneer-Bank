79d55f7cec8d8762127dd75592117e13eaf4cea9
MD Abu Bakar Siddique <2021331009@student.sust.edu>
2026-01-09 17:45:58 +0600

    Added login page

---
+++ script.js
@@ -0,0 +1,4 @@
+const loginbtn=document.getElementById("login").addEventListener("click", function(){
+    const loginArea=document.getElementById("bank-area");
+    loginArea.style.display="none";
+})
