540dd1cd120c6af0a707288850b1bbdbec85f814
MD Abu Bakar Siddique <2021331009@student.sust.edu>
2026-01-09 19:32:42 +0600

    Added transaction page

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const loginbtn=document.getElementById("login").addEventListener("click", function(){
     const loginArea=document.getElementById("bank-area");
     loginArea.style.display="none";
+    const transactionArea=document.getElementById("transaction-area");
+    transactionArea.style.display="block";
 })
