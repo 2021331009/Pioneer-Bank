c8e7d20c467fcf82c2e4ffd59d1f1309572a7cfa
MD Abu Bakar Siddique <2021331009@student.sust.edu>
2026-01-09 20:20:03 +0600

    Added withdraw and balance button

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+//login button event handler
 const loginbtn=document.getElementById("login").addEventListener("click", function(){
     const loginArea=document.getElementById("bank-area");
     loginArea.style.display="none";
     const transactionArea=document.getElementById("transaction-area");
     transactionArea.style.display="block";
-})
+});
+
+//deposit button event handler
+const depositbtn=document.getElementById("addDeposit").addEventListener("click", function(){
+    const depositAmount=document.getElementById("depositAmount").value;
+    const depositNumber=parseFloat(depositAmount);
+
+    const currentDeposit=document.getElementById("currentDeposit").innerText;
+    const currentDepositNumber=parseFloat(currentDeposit);
+    const totalDeposit=currentDepositNumber + depositNumber;
+    document.getElementById("currentDeposit").innerText=totalDeposit;
+    document.getElementById("depositAmount").value="";
+});
